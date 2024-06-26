2d7ecfb511b03fd81a66e7442cff5751d262af93
smecik48 <piotrsmet@interia.pl>
2024-06-26 19:21:42 +0200

    dodanie styli

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">Wpisy</button>
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpis">Dodaj</button>
+        <h1 class="text-blue-600">Wpisy na bloga</h1>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+        <div class="grid mx-6 gap-4 my-4">
+        <div v-for="(wpis, index) in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+        </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpis" class="bg-blue-600 rounded text-white p-4">Dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -22,6 +31,9 @@ import { dzien2_backend } from 'declarations/dzien2_backend/index';
             },
             async pobierzWpisy(){
                 this.wpisy = await dzien2_backend.odczytaj_wpisy();
+            },
+            async mounted(){
+                this.pobierzWpisy();
             }
         }
     }
